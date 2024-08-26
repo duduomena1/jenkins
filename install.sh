44414bfb57c4957b3af8999d9c0d8dbe50bf91e7
duduomena1 <duduomena.ec@icloud.com>
2024-08-26 15:10:25 -0300

    UPDATE

--- install.sh
+++ install.sh
@@ -29,6 +29,11 @@ case $choice in
         sudo install -m 0755 -d /etc/apt/keyrings
         sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
         sudo chmod a+r /etc/apt/keyrings/docker.asc
+        echo \
+            "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+            $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+            sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+        sudo apt-get update
         sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
         
         # Instalando K3D
@@ -60,14 +65,13 @@ case $choice in
         break;;
     
     2)  echo "2 - Instalando Docker"
-        for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do 
-            sudo apt-get remove -y $pkg
-        done
+        for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove -y $pkg; done
         sudo apt-get update
         sudo apt-get install -y ca-certificates curl
         sudo install -m 0755 -d /etc/apt/keyrings
         sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
         sudo chmod a+r /etc/apt/keyrings/docker.asc
+        sudo apt-get update
         sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
         break;;
         
