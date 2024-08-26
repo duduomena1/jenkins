133a30d7186ecdc1cf5efb3f8ec5a2fa2150671d
duduomena1 <duduomena.ec@icloud.com>
2024-08-26 15:15:08 -0300

    update

--- install.sh
+++ install.sh
@@ -62,7 +62,7 @@ case $choice in
         echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/" | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
         sudo apt-get update
         sudo apt-get install -y jenkins
-        break;;
+;;
     
     2)  echo "2 - Instalando Docker"
         for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove -y $pkg; done
@@ -73,7 +73,7 @@ case $choice in
         sudo chmod a+r /etc/apt/keyrings/docker.asc
         sudo apt-get update
         sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-        break;;
+;;
         
     3)  echo "3 - Instalando kubectl"
         sudo apt-get update && sudo apt-get install -y curl
@@ -83,7 +83,7 @@ case $choice in
         sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
         chmod +x kubectl
         mkdir -p ~/.local/bin
-        mv ./kubectl ~/.local/bin/kubectl;;
+;;
     
     4)  echo "4 - Instalando k3d"
         curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
@@ -101,13 +101,13 @@ case $choice in
         echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/" | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
         sudo apt-get update
         sudo apt-get install -y jenkins
-        break;;
+;;
     
     6)  echo "6 - Instalando Terraform"
         wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
         echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
         sudo apt update && sudo apt install -y terraform
-        break;;
+;;
     
     7)  echo "7 - Saindo"
         exit;;
