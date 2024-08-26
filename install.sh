6a8cc1ef558b530b61587e41d89e3005a57d1d80
duduomena1 <duduomena.ec@icloud.com>
2024-08-26 15:17:00 -0300

    .

--- install.sh
+++ install.sh
@@ -104,7 +104,7 @@ case $choice in
 ;;
     
     6)  echo "6 - Instalando Terraform"
-        wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+        wget -y -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
         echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
         sudo apt update && sudo apt install -y terraform
 ;;
